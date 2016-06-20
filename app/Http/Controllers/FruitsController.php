c82ffdd64cdd01421be72f3c566a44e8b2aefb49
Bill Mitchell <wkmitch@gmail.com>
2016-06-21 00:49:20 +0100

    first commit

---
+++ app/Http/Controllers/FruitsController.php
@@ -0,0 +1,60 @@
+<?php namespace App\Http\Controllers;
+
+use App\Fruit;
+use App\Http\Requests;
+use Dingo\Api\Routing\Helpers;
+use App\Transformers\FruitsTransformer;
+
+class FruitsController extends Controller
+{
+
+	use Helpers;
+
+	public function index()
+	{
+	    $fruits = Fruit::all();
+
+        return $this->collection($fruits, new FruitsTransformer);
+	}
+
+	public function show($id)
+	{
+	    $fruit = Fruit::where('id', $id)->first();
+
+	    if ($fruit) {
+	        return $this->item($fruit, new FruitsTransformer);
+	    }
+
+	    return $this->response->errorNotFound();
+	}
+
+	public function store(Requests\StoreFruitRequest $request)
+	{
+	    if (Fruit::Create($request->all())) {
+	        return $this->response->created();
+	    }
+
+	    return $this->response->errorBadRequest();
+	}
+	
+	/**
+	 * Remove the specified fruit.
+	 *
+	 * @param  int $id
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+	public function destroy($id)
+	{
+	    $fruit = Fruit::find($id);
+
+	    if ($fruit) {
+	        $fruit->delete();
+	        return $this->response->noContent();
+	    }
+
+	    return $this->response->errorBadRequest();
+	}
+
+
+}
